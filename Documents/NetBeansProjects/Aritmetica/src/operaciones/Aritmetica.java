fbcf11d66b6957345dc26ce3037d4697a2cc2a7a
Xiomara Giraldo <xiomaragiraldo221@gmail.com>
2021-07-26 16:20:55 -0500

    Metodo

---
+++ Documents/NetBeansProjects/Aritmetica/src/operaciones/Aritmetica.java
@@ -0,0 +1,18 @@
+
+package operaciones;
+
+public class Aritmetica {
+    
+    int a;
+    int b;
+    
+    //Metodo
+    public void sumar(){
+        int resultado = a + b;
+        System.out.println("resultado = " + resultado);
+    }
+    public int sumarConRetorno(){
+        return a + b;
+    }
+    
+}
